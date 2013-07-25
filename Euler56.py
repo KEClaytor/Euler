fc5f62f2c0bcd2c78da19b09c01f7a909722cc51
Kevin <ke.claytor@gmail.com>
2013-07-25 09:29:28 -0400

    Solution to 56.

---
+++ Euler56.py
@@ -0,0 +1,17 @@
+# Find the maximum digit sum of a^b, where a,b <= 100
+
+from eulermath import int2array
+
+nmax = 100
+smax = 0
+for a in range(1,nmax+1):
+    for b in range(1,nmax+1):
+        p = a**b
+        s = sum(int2array(p))
+        #print "%d^%d = %d, digit sum = %d" % (a,b,p,s)
+        if s > smax:
+            smax = s
+            #print "found a larger sum"
+    print "%d/%d complete" % (a,nmax)
+
+print "The maximum digit sum is: %d" % (smax)
