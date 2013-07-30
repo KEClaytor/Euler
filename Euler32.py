adc5c09c8b78e9e9231cc441d27b81279ea2fc6c
Kevin <ke.claytor@gmail.com>
2013-07-30 10:15:51 -0400

    In-progres on 32. Seems to be working correctly, but is not giving the correct answer.

--- Euler32.py
+++ Euler32.py
@@ -1,7 +1,44 @@
-import itertools
+# Find the number of unique ways we can write out
+#  all 1-9 pandigital products, eg; 39*186=7254
 
-intstr = '123456789'
-permlist = itertools.permutations(intstr,9)
-for x in permlist:
-    print x
+from eulermath import makepandigital, array2int
 
+# Splits up the pandigital number into it's components
+#  And checks them for a valid product rule
+def checksub(pan):
+    prods = []
+    for ii in range(1,8):
+        a = array2int(pan[0:ii])
+        for jj in range(ii+1,9):
+            b = array2int(pan[ii:jj])
+            c = array2int(pan[jj::])
+            #print "%d" % (array2int(pan))
+            #print "%d * %d =? %d -> %d" % (a,b,c,a*b==c)
+            if (a*b==c):
+                prods.append((a,b,c))
+    return prods
+
+plist = makepandigital(9)
+prodlist = []
+for pan in plist:
+    prods = checksub(pan)
+    if len(prods) > 0:
+        print ""
+        print "Found %d product combinations for the pandigital %d" % (len(prods),array2int(pan)) 
+        # Check that each new element isn't already in our list before we add it
+        for prod in prods:
+            no = (prod[0], prod[1], prod[2])
+            ro = (prod[1], prod[0], prod[2])
+            na = 0
+            if (no in prodlist):
+                print repr(no) + " no already in" 
+            if (ro in prodlist):
+                print repr(ro) + " ro already in" 
+            if (no not in prodlist) and (ro not in prodlist):
+                print "appending " + repr(no)
+                prodlist.append(no)
+                na += 1
+            print "Appended %d of those" % (na)
+
+print prodlist
+print "There are %d unique 1-9 pandigital products." % (len(prodlist))
