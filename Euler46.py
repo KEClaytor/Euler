7bd7baf60ae2de32da692303345c219b5df3e001
Kevin <ke.claytor@gmail.com>
2013-08-07 16:35:14 -0400

    Solution to 46.

---
+++ Euler46.py
@@ -0,0 +1,42 @@
+# Find the smallest odd composite that cannot be written as
+#  the sum of a prime and twice a square:
+# OC =?= P + 2x(i)^2
+from eulermath import isprime
+
+# make an odd composite from 1 <= j <= i
+def moc(i,j):
+    if j > i:
+        j = i
+        print "invalid value of j, trimming to i"
+    return (2*i+1)*(2*j+1)
+
+# Tests to see if this number can be written as
+#  the sum of a prime and twice a square
+def isPp2Sq(num):
+    i = 1
+    rval = 0
+    while 2*(i**2) < num:
+        res = num - 2*(i**2)
+        if isprime(res):
+            rval = 1 # We can do the decomposition
+            break
+        i += 1
+    return rval
+
+found = False
+i = 1
+j = 1
+while not found:
+    # Test to see if we need to increment i
+    if j > i:
+        i +=1
+        j = 1
+    # If this odd composite cannot be decomposed
+    oc = moc(i,j)
+    if not isPp2Sq(oc):
+        found = True
+    # Increment j
+    j += 1
+
+print "The smallest odd composite number that cannot be decomposed"
+print "  into a prime plus twice a square is: %d" % (oc)
