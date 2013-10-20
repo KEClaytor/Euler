ef0130d89611e33d15bae1492c5a5f14eb1d2422
KE Claytor <ke.claytor@gmail.com>
2013-10-20 16:56:28 -0400

    Finally fixed solution to 58. Precomputing the primes really helps, then you can brute force up from side 1 so you don't miss the drop. I was getting too fancy with 58f.

--- Euler58.py
+++ Euler58.py
@@ -2,23 +2,30 @@
 # for what side length does the ratio of primes fall to <10%?
 
 from __future__ import division
-from eulermath import isprime
+from eulermath import primesieve
 
-smax = 11
-side = 3
-nprime = 0
-nnotprime = 0
-fracprime = 1
-while (fracprime > .1):
+# Get all the diagonal values for a given (odd) side length
+def getdiags(side):
     offset = (side-2)**2 + (side-1)
     diag = range(offset,side**2+1,side-1)
-    for elem in diag:
-        if isprime(elem):
-            nprime += 1
-        else:
-            nnotprime += 1
-    fracprime = nprime/nnotprime
-    print side, fracprime
-    side += 2
+    return diag
 
-print "Side length for which the ratio of primes is < 10% is: " + repr(side-2)
+if __name__ == "__main__":
+    ntotalprime = 0  # Starting on side length 1 we have zero primes
+    ndiag = 1        # with only 1
+    pside = 26507
+    side = 3
+    fracprime = 1.00
+    # Only increment this if we have to
+    print "Pre-computing prime list, please hold on..."
+    primes = set(primesieve(pside**2+1))
+    while side < 50 or fracprime > 0.1:
+        diag = getdiags(side)
+        nprime = len(primes.intersection(diag))
+        ntotalprime += nprime
+        ndiag += 4
+        fracprime = ntotalprime/ndiag
+        print side, fracprime
+        side += 2
+
+    print "Side length for which the ratio of primes is < 10% is: " + repr(side-2)
