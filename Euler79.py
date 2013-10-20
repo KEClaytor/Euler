753a042efcb95e29c44af810875002dbd13487b2
KE Claytor <ke.claytor@gmail.com>
2013-10-20 17:31:33 -0400

    Solution to 79. Requires a bit of pencil-work.

--- Euler79.py
+++ Euler79.py
@@ -9,29 +9,6 @@ from eulermath import int2array, array2int, hist
 # Quick 2-level flatten
 flatten = lambda arr: [x for l in arr for x in l]
 
-# Test to see if the elments of two lists are equal
-def listeq(a,b):
-    eq = False
-    if len(a) == len(b):
-        eq = True
-        for ii in range(len(a)):
-            if a[ii] != b[ii]:
-                eq = False
-                break
-    return eq
-
-# Check that the list test can generate the observed keys
-def checkval(test,keys):
-    check = True
-    for key in keys:
-        ind = []
-        for x in key:
-            ind.append(test.index(x))
-        if not listeq(ind,ind.sort()):
-            check = False
-            break
-    return check
-
 # Trim values based on frequency
 # totrim is a tuple of lists, and counts is a list
 #  all these lists must be the same length
@@ -46,12 +23,9 @@ def trim(totrim,counts,minv=0,maxv="inf"):
         trimmed.append(tsublist)
     return trimmed
 
-# Make some potential keys
-def make_potential_keys(start=0,end=0,midpairs=[00]):
-    potential_keys = make_potential_keys(start=7,end=0,midpairs=flatten([t12[0],t23[0]]))
-    
 # Import the data
 sub = []
+p123 = []
 p12 = []; p23 = []
 p1 = []; p2 = []; p3 = []
 fnames = open('Euler79.dat')
@@ -66,6 +40,7 @@ for line in fnames:
     # Pairs
     p12.append(array2int(val[0:2]))
     p23.append(array2int(val[1:3]))
+    p123.append(array2int(val))
 
 # Flatten out the sublist and take all unique elements
 fsl = list(set([x for arr in sub for x in arr]))
@@ -76,13 +51,24 @@ v2, c2 = hist(p2)
 v3, c3 = hist(p3)
 v12, c12 = hist(p12)
 v23, c23 = hist(p23)
+v123, c123 = hist(p123)
 
+print "We need to use all these:"
 print fsl
+print "max histograms"
 print trim([v1,c1],c1,2)
 print trim([v2,c2],c2,2)
 print trim([v3,c3],c3,2)
 print trim([v12,c12],c12,2)
 print trim([v23,c23],c23,2)
+print "all histograms"
+print [v1,c1]
+print [v2,c2]
+print [v3,c3]
+print [v12,c12]
+print [v12,c12]
+print [v23,c23]
+print [v123,c123]
 # Looking at the histograms, we can see
 #   that there are a few numbers;
 #    7 - which appears most frequently in the
@@ -93,9 +79,11 @@ print trim([v23,c23],c23,2)
 # These are the likely ending numbers
 # We will then have to sort the others based on their pairs
 
-t12 = trim([v12,c12],c12,2)
-t23 = trim([v23,c23],c23,2)
-potential_keys = make_potential_keys(start=7,end=0,midpairs=flatten([t12[0],t23[0]]))
-
-print potential_keys
+print "Now stare at this histograms... "
+print "7 and 0 pretty obviously begin and end."
+print "3 is 2nd spot, as it never is chosen as an end, and 9 is the second to last as it never begins."
+print "the work through the other options... 62 appears and 26 never does..."
+print "...scratch head..."
+print
+print "and discover the answer; 73162890"
 
