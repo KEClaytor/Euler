085472af767de533dc22bcd484348b8168039889
KE Claytor <ke.claytor@gmail.com>
2013-10-24 19:00:14 -0400

    Solution to 69.

---
+++ Euler69.py
@@ -0,0 +1,16 @@
+# Find the largest value of n/\phi(n) for n <= 1M
+
+from __future__ import division
+from eulermath import eulerphi
+
+nmax = 0
+rmax = 1
+print "n phi(n) n/phi(n)"
+for n in range(1,10**6+1):
+    pn = eulerphi(n)
+    if n/pn > rmax:
+        nmax = n
+        rmax = n/pn
+        print "New max: " + repr(n)
+
+print "The maximum ratio occurs for n = %d" % (nmax)
