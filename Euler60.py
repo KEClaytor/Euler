48c6b185c29daf641eca816a1ffbae7c333bf846
KE Claytor <ke.claytor@gmail.com>
2014-06-13 14:28:03 -0400

    Turns out more seeds were needed. Now obtaining a combination with primes < 10,000

--- Euler60.py
+++ Euler60.py
@@ -1,25 +1,71 @@
-# Euler 60
-# Find the list of five primes whereby one can take any two
-#  and concatenate them in any order and obtain a prime.
+# Euler 60 - Find the smallest sum of 5 primes
+#            that can be concatenated into
+#            other primes
 
-from eulermath import isprimeFermat as isprime, gen_primes
+from eulermath import isprimeFermat as isprime
+from eulermath import gen_primes
+import itertools
+from itertools import chain
 
-if __name__=="__main__":
+# What primes can be concatenated to form this prime?
+def checkPrime(prime, pairs):
+    vs = str(prime)
+    cps = []
+    for pair in pairs:
+        possible = True
+        # We have to work for all elements in the pair
+        #  in order to form a new tuple
+        for elem in pair:
+            a = int(str(elem) + vs)
+            b = int(vs + str(elem))
+            # A prime never begins, nor ends with zero
+            #  so we don't have to check that we lost digits
+            if not isprime(a) or not isprime(b):
+                possible = False
+        if possible:
+            new = (prime,) + pair
+            cps.append(new)
+    return cps
+
+if __name__ == "__main__":
+    primeListLength = 3
+    # First make our prime list
+    maxprime = 100000
     primes = gen_primes()
-    print primes
-    # Hint: we'll be expanding this list
-    sequence = [3, 7]
+    # Seed us with the pair (3,7) which forms 37 & 73, both prime
+    # Search through the higher primes, seeing if they share the
+    #   property for all primes in this set
+    #   This allows us to form the length 3, 4, and 5 sets.
+    pairs = [(3,), (7,)]
+    length5pairs = []
     for prime in primes:
-        works = True
-        for cp in sequence:
-            cata = int(str(prime) + str(cp))
-            catb = int(str(cp) + str(prime))
-            if not isprime(cata) or not isprime(catb):
-                works = False
-        if works:
-            sequence.append(prime)
-            print sequence
-        if len(sequence) == 5:
+        if prime > maxprime:
             break
-    print "Found a length-5 sequence."
-    print sequence, sum(sequence)
+        if prime < 8:
+            continue
+        # Get the possible lists we have
+        new = checkPrime(prime, pairs)
+        if new:
+            for item in new:
+                print "adding: " + repr(item)
+                pairs.append(item)
+                if len(item) > 4:
+                    length5pairs.append(item)
+            # Remove duplicates
+            pairs = list(set(pairs))
+            # Sort by length
+            pairs = sorted(pairs, key=lambda val: len(val))
+            #print "we now have:"
+            #print pairs
+            #if len(pairs[-1]) == 5:
+            #    break
+
+    #print "First length-5 tuple found. It's probably the smallest sum:"
+    #print pairs[-1], sum(pairs[-1])
+
+    # Look at all of the length-5 pairs we found
+    print "Found %d length-5 tuples with elements < %d. Here they are:" % (len(length5pairs), maxprime)
+    # Sort the pairs by their sum
+    length5pairs = sorted(length5pairs, key=lambda val: sum(val))
+    for pair in length5pairs:
+        print pair, sum(pair)
