05ace76912e08d5409d1b6a8387d0ee4292ce9dc
Kevin <ke.claytor@gmail.com>
2014-07-30 14:42:08 -0400

    Speedup via checking n for narrower range, using prime factors

--- Euler71.py
+++ Euler71.py
@@ -2,12 +2,13 @@
 
 # ARGH current build doesn't have this
 #from future import division
-from eulermath import factors
+from eulermath import primefactors
 from math import floor
 
 def is_reduced_proper_fraction(num, denom):
-    common_factors = set(factors(num)) & set(factors(denom))
-    if len(common_factors) == 1:
+    common_factors = set(primefactors(num)) & \
+        set(primefactors(denom))
+    if len(common_factors) == 0:
         return True
     else:
         return False
@@ -21,21 +22,29 @@ def less_than_three_sevenths(num, denom):
 
 # Maximum denominator
 maxd = 1000000.
-closest_num = 0.
-closest_denom = 1.
+closest_num = 0.0
+closest_denom = 1.0
+diff = 3.0/7.0 - closest_num/closest_denom
 
 print "Searching with maximum denominator: %d" % (maxd)
-# We can rely on condition evaluation order in python
-for denom in xrange(maxd+1, 0, -1):
-    # Evaluate this backwards until we find
-    #  a reduced proper fraction < 3/7
-    for num in xrange(floor(denom*3.0/7.0), 0, -1):
-        if is_reduced_proper_fraction(num, denom):
-            if num*1.0/(denom*1.0) > closest_num*1.0/(closest_denom*1.0) and \
-                num*1.0/(denom*1.0) < 3.0/7.0:
-                closest_num = num
-                closest_denom = denom
-                print "Updating with %d/%d" % (num, denom)
-            break
+# Evaluate this backwards until we find
+#  a reduced proper fraction < 3/7
+for denom in xrange(maxd+1):
+    # Only evaluate numerators that give us:
+    #  a result < 3/7 (n_max)
+    #  a result > closet_num/closest_denom (n_min)
+    n_max = floor(denom*3.0/7.0)
+    n_min = floor(closest_num/closest_denom * (denom*1.0))
+    if n_max > n_min:
+        print "d: %d\t n_min: %d\t n_max: %d\t delta_n: %d" %(denom, n_min, n_max, n_max-n_min)
+        for num in xrange(n_max, n_min, -1):
+            if is_reduced_proper_fraction(num, denom):
+                if num*1.0/(denom*1.0) > closest_num/closest_denom and \
+                    num*1.0/(denom*1.0) < 3.0/7.0:
+                    closest_num = num*1.0
+                    closest_denom = denom*1.0
+                    diff = 3.0/7.0 - closest_num/closest_denom
+                    print "Updating with %d/%d" % (num, denom)
+                break
 
 print "The closest reduced proper fraction is %d/%d" % (closest_num, closest_denom)
