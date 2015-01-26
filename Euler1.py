eac2f1fcea0ea349db5e5d02fc0fe859d94c2ad7
KE Claytor <ke.claytor@gmail.com>
2015-01-26 17:35:53 -0500

    Slowly bringing code into compliance with pep-8 and Python 3.

--- Euler1.py
+++ Euler1.py
@@ -1,10 +1,8 @@
-n = input("Max bound?: ")
-total = 0
-#list = []
-for ii in range(1,n):
-    if (ii % 3 == 0) | (ii % 5 == 0):
-        total += ii
-        #list.append(ii)
+if __name__ == "main":
+    n = input("Max bound?: ")
+    total = 0
+    for ii in range(1, n):
+        if (ii % 3 == 0) | (ii % 5 == 0):
+            total += ii
 
-print "Sum of multiples of 3 or 5 up to (not including)", n, "is:", total
-#print "The multiples of 3 or 5 below ", n, " is: ", list
+    print "Sum of multiples of 3 or 5 up to (not including)", n, "is:", total
--- Euler2.py
+++ Euler2.py
@@ -3,22 +3,23 @@
 
 # Brute force method
 
-#fmax = 90 # should give 44
-fmax = 4000000
-fibsum = 0
+if __name__ == "main":
+    # fmax = 90 # should give 44
+    fmax = 4000000
+    fibsum = 0
 
-# The starting values
-# Note that the third is always even
-fa = 1
-fb = 1
-fc = 2
+    # The starting values
+    # Note that the third is always even
+    fa = 1
+    fb = 1
+    fc = 2
 
-#print "Even terms in the Fib sequence are:", fa, fb, fc
-while (fc < fmax):
-    fibsum += fc
-    fa = fc + fb
-    fb = fa + fc
-    fc = fa + fb 
-    #print fa, fb, fc
+    # print "Even terms in the Fib sequence are:", fa, fb, fc
+    while (fc < fmax):
+        fibsum += fc
+        fa = fc + fb
+        fb = fa + fc
+        fc = fa + fb
+        # print fa, fb, fc
 
-print "The sum of fibonacci numbers <", fmax, "is:", fibsum
+    print "The sum of fibonacci numbers <", fmax, "is:", fibsum
